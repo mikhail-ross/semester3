913d7aa9a4ff0da102a018f069e9081b16bc0262
Mikhail <rosseevm@gmail.com>
2021-09-06 13:22:55 +0300

    добавил функцию func через которую выводится HW!

--- 1-lab.cpp
+++ 1-lab.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
+
+int func (int x)
+{
+	if (x!=0)
+		x=0;
+	return x;
+}
+
 int main()
 {
 	int x;
 	cin>>x;
-	if (x==0)
+	if (func(x)==0)
 		cout<<"Hello, World!"<<endl; //написано сообщение под вывод в консоль и указано начало с новой строчки
 return 0;
 }
